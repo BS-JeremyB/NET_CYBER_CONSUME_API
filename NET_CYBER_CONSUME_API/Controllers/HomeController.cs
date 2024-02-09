6e3cf9e92d88966114301c2a77a8213224ad00c1
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-02-09 17:03:40 +0100

    Ajoutez des fichiers projet.

---
+++ NET_CYBER_CONSUME_API/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET_CYBER_CONSUME_API.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace NET_CYBER_CONSUME_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://v2.jokeapi.dev/joke/");
+        }
+
+        public IActionResult Index()
+        {
+            //string kindOfJoke = "Programming";
+            //HttpResponseMessage response = _httpClient.GetAsync($"{kindOfJoke}?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=single").Result;
+            //if(response.IsSuccessStatusCode)
+            //{
+            //    JokeModel joke = response.Content.ReadFromJsonAsync<JokeModel>().Result;
+            //    return View(joke);
+            //}
+
+            HttpResponseMessage response = _httpClient.GetAsync($"Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=single").Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string responseText = response.Content.ReadAsStringAsync().Result;
+                JokeModel joke = JsonConvert.DeserializeObject<JokeModel>(responseText);
+                return View(joke);
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NET_CYBER_CONSUME_API/Models/JokeModel.cs
@@ -0,0 +1,7 @@
+﻿namespace NET_CYBER_CONSUME_API.Models
+{
+    public class JokeModel
+    {
+        public string Joke { get; set; }
+    }
+}
